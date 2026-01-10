8883b0dae6f2ae5c98653540b6b72c87c01c3d40
Himanshuyadav1 <himanshu.tj@gmail.com>
2026-01-11 03:54:59 +0530

    Solved Day 9 Problem 3

--- Day 09/script.js
+++ Day 09/script.js
@@ -38,33 +38,70 @@
 // Solution 2:
 // -----------------
 
+// function handleSubmit(e) {
+//     e.preventDefault();
+
+//     const inputString = e.target[0].value;
+//     const subString = e.target[1].value;
+//     const resultPara = document.getElementById('result');
+//     const stringLength = inputString.length;
+//     const subStringLength = subString.length;
+//     let isSubString;
+//     let index = -1;
+
+//     for (let i = 0; i <= stringLength - subStringLength; i++) {
+//         isSubString = true;
+
+//         for (let j = 0; j < subStringLength; j++) {
+//             if (inputString[i + j] !== subString[j]) {
+//                 isSubString = false;
+//                 break;
+//             }
+//         }
+
+//         if (isSubString) {
+//             index = i;
+//         }
+//     }
+    
+//     resultPara.textContent = `Found at index ${index}`;
+//     e.target[0].value = "";
+//     e.target[1].value = "";
+// }
+
+// -----------------
+
+
+// Solution 3:
+// -----------------
+
 function handleSubmit(e) {
     e.preventDefault();
 
     const inputString = e.target[0].value;
     const subString = e.target[1].value;
     const resultPara = document.getElementById('result');
-    const stringLength = inputString.length;
+    const fullRotation = inputString + inputString;
+    const fullRotationLength = fullRotation.length;
     const subStringLength = subString.length;
-    let isSubString;
-    let index = -1;
+    let isRotationString;
 
-    for (let i = 0; i <= stringLength - subStringLength; i++) {
-        isSubString = true;
+    for (let i = 0; i <= fullRotationLength - subStringLength; i++) {
+        isRotationString = true;
 
         for (let j = 0; j < subStringLength; j++) {
-            if (inputString[i + j] !== subString[j]) {
-                isSubString = false;
+            if (fullRotation[i + j] !== subString[j]) {
+                isRotationString = false;
                 break;
             }
         }
 
-        if (isSubString) {
-            index = i;
+        if (isRotationString) {
+            break;
         }
     }
     
-    resultPara.textContent = `Found at index ${index}`;
+    resultPara.textContent = `${isRotationString ? 'Rotation' : 'Not Rotation'}`;
     e.target[0].value = "";
     e.target[1].value = "";
 }
